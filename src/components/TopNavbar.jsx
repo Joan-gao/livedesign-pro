317c0534092b3dbff8a3e50e25f00e6a10e05f8f
vi <vietdoan.vqd@gmail.com>
2024-06-16 08:01:04 +1000

    Add star Icon for AI on main page

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -2,10 +2,23 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTv, faSearch } from '@fortawesome/free-solid-svg-icons';
 
+// Import AI Icons
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
 const TopNavbar = () => {
+  // Styling for AI Icons Div
+  const topLeftIcons = {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: '12px'
+  };
+    
   return (
     <div className="top-navbar">
-      <FontAwesomeIcon icon={faTv} className='icon'/>
+      <div style={topLeftIcons}>
+        <FontAwesomeIcon icon={faTv} className='icon'/>
+        <FontAwesomeIcon icon={faStar} className='icon'/>
+      </div>
       <h2>Following  |   <span>For You</span></h2>
       <FontAwesomeIcon icon={faSearch} className='icon'/>
     </div>
