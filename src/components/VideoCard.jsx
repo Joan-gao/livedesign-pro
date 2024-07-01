8230554f9bd566e376e72df3517329feee040d5e
amandaliberaann <amandaliberaann@gmail.com>
2024-07-01 09:58:52 +0800

    clean working tree

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -8,21 +8,34 @@ const VideoCard = (props) => {
   const videoRef = useRef(null);
 
   useEffect(() => {
-    if (autoplay) {
-      videoRef.current.play();
-    }
+    const handleUserInteraction = () => {
+      if (autoplay && videoRef.current) {
+        videoRef.current.play().catch((error) => {
+          console.error('Error attempting to play video:', error);
+        });
+      }
+      document.removeEventListener('click', handleUserInteraction);
+    };
+
+    document.addEventListener('click', handleUserInteraction);
+
+    return () => {
+      document.removeEventListener('click', handleUserInteraction);
+    };
   }, [autoplay]);
 
   const onVideoPress = () => {
     if (videoRef.current.paused) {
-      videoRef.current.play();
+      videoRef.current.play().catch((error) => {
+        console.error('Error attempting to play video:', error);
+      });
     } else {
       videoRef.current.pause();
     }
   };
 
   return (
-    <div className="video">
+    <div className="video">  
       {/* The video element */}
       <video
         className="player"
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,6 +4,11 @@ import VideoCard from '../components/VideoCard';
 import BottomNavbar from '../components/BottomNavbar';
 import TopNavbar from '../components/TopNavbar';
 
+import { useLocation } from "react-router-dom";
+import DesignSection from '../components/DesignPage/DesignSection';
+import FooterLeft from '../components/FooterLeft';
+import FooterRight from '../components/FooterRight';
+
 // Define the structure of a video object
 interface Video {
   url: string;
@@ -69,6 +74,10 @@ const Index: React.FC = () => {
   const [videos, setVideos] = useState<Video[]>([]);
   const videoRefs = useRef<(HTMLVideoElement | null)[]>([]);
 
+  const location = useLocation();
+  const imageData = location.state?.image;
+  const audio = location.state?.audio;
+
   useEffect(() => {
     setVideos(videoUrls);
   }, []);
@@ -115,6 +124,26 @@ const Index: React.FC = () => {
   return (
     <div className="container">
       <TopNavbar className="top-navbar" />
+      
+      {imageData ? (
+        <div className="video">
+          <div className='player'>
+              <DesignSection imageData={imageData} />           
+          </div>
+
+          <div className="bottom-controls">
+            <div className="footer-left">
+              {/* The left part of the container */}
+              <FooterLeft username={'Test'} description={'Test'} song={audio}/>
+            </div>
+            <div className="footer-right">
+              {/* The right part of the container */}
+              <FooterRight likes={0} shares={0} comments={0} saves={0} profilePic={'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/eace3ee69abac57c39178451800db9d5~c5_100x100.jpeg?x-expires=1688479200&x-signature=wAkVmwL7lej15%2B16ypSWQOqTP8s%3D'} />
+            </div>
+          </div>
+        </div>
+      ) : null}
+
       {/* Here we map over the videos array and create VideoCard components */}
       {videos.map((video, index) => (
         <VideoCard
@@ -130,6 +159,7 @@ const Index: React.FC = () => {
           profilePic={video.profilePic}
           setVideoRef={handleVideoRef(index)}
           autoplay={index === 0}
+          imageData={imageData}
         />
       ))}
       <BottomNavbar className="bottom-navbar" />
